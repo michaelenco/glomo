4f6ae9c43c3af0890f9656aa3ecdbe7e05c70603
gcorn <michael.enco@gmail.com>
2015-11-25 10:17:17 +0300

    --

--- ejabberd_modules/mod_iq_register.erl
+++ ejabberd_modules/mod_iq_register.erl
@@ -4,57 +4,43 @@
 
 -behaviour(gen_mod).
 
--define(NS_REG, <<"jabber:iq:reg123">>).
+-define(NS_REG, <<"jabber:iq:register_phone">>).
 
 -export([start/2, 
 	 stop/1, 
-	 stream_feature_register/2, 
-	 unauthenticated_iq_register/4,
-	 process_iq/3]).
+	 unauthenticated_iq_register/4]).
 
 -include("ejabberd.hrl").
 -include("logger.hrl").
-
 -include("jlib.hrl").
-
 -include("mod_privacy.hrl").
 
-
-
 start(Host, Opts) ->
-	io:format("starting iq registration module!~n~n~n~n~n~n",[]),
-	gen_iq_handler:add_iq_handler(ejabberd_local, Host, ?NS_REG, ?MODULE, process_local_iq, parallel),
-	%ejabberd_hooks:add(c2s_stream_features, Host, ?MODULE, stream_feature_register,50),
 	ejabberd_hooks:add(c2s_unauthenticated_iq, Host, ?MODULE, unauthenticated_iq_register, 50),
 	ok.
 
 stop(Host) ->
-	gen_iq_handler:remove_iq_handler(ejabberd_local, Host,?NS_REG).
-%ejabberd_hooks:delete(c2s_unauthenticated_iq, Host,?MODULE, unauthenticated_iq_register, 50).
+	ejabberd_hooks:delete(c2s_unauthenticated_iq, Host,?MODULE, unauthenticated_iq_register, 50).
 
-stream_feature_register(Acc, _Host) -> 
-	io:format("stream_feature_register handler! ~n~n~n~n~n~n",[]),
-	[#xmlel{name = <<"iq_register">>,
-		attrs = [{<<"xmlns">>, ?NS_REG}],
-		children = []}
-	 | Acc].
 
-process_iq(_From, _To, #iq{type = Type, sub_el = SubEl} = IQ) ->
-	io:format("process_iq! ~n~n~n~n~n~n",[]),
-	IQ#iq{type = result,
-	      sub_el =
-	      [#xmlel{name = <<"query">>,
-		      attrs =
-		      [{<<"xmlns">>, ?NS_REG},
-		       {<<"seconds">>,
-			"Hello"}],
-		      children = []}]}.
-
-unauthenticated_iq_register(Acc, Server, #iq{xmlns = ?NS_REG} = IQ, IP) -> 
-	io:format("unauthenticated_iq_register! ~n~n~n~n~n~n",[]),
-	#xmlel{name = <<"iq_register">>,
-	       attrs = [{<<"message">>, "Fuck you erlnag"}],
-	       children = []};
+unauthenticated_iq_register(Acc, Server, #iq{xmlns = ?NS_REG, sub_el = SubEl} = IQ, IP) -> 
+	PhoneTag = xml:get_subtag(SubEl, <<"phone">>),
+	if
+		(PhoneTag /= false) -> 
+			io:format("phone tag: ~w ~n~n~n",[PhoneTag]),
+			{xmlel,_,_,Children} = PhoneTag,
+			PhoneNumber = xml:get_cdata(Children),
+			io:format("here we have to send some sms to ~s number~n~n~n",[PhoneNumber]),
+%			case ejabberd_auth:try_register(Username, Host,Password)
+%			of
+%				{atomic, Res} ->
+%					{success, Res, {Username, Host, Password}};
+%				Other -> Other
+%			end;
+			jlib:iq_to_xml(IQ#iq{type = result,sub_el = []});
+		true ->
+			jlib:iq_to_xml(IQ#iq{type = error,sub_el = [SubEl, ?ERR_NOT_ALLOWED]})
+	end;
 unauthenticated_iq_register(Acc, _Server, _IQ, _IP) ->
 	Acc.
 
--- tcp_client.js
+++ tcp_client.js
@@ -11,7 +11,7 @@ sock.on('connect', function(){
 	console.log("connected")
 	sock.write("<?xml version=\"1.0\"?>")
 	sock.write('<stream:stream xmlns:stream="http://etherx.jabber.org/streams" version="1.0" xmlns="jabber:client" to="localhost" xml:lang="en" xmlns:xml="http://www.w3.org/XML/1998/namespace">')
-	sock.write("<iq type='get'><query xmlns='jabber:iq:reg123'/></iq>")
+	sock.write("<iq type='get'><query xmlns='jabber:iq:register_phone'><phone>+79817852735</phone></query></iq>")
   	sock.pipe(process.stdout)
 	process.stdin.pipe(sock)
 })

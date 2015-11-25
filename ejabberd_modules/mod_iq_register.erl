c3ed02d7669bb2a8c10cda5a8e6dd06d9768083f
gcorn <michael.enco@gmail.com>
2015-11-25 07:42:58 +0300

    sync

--- ejabberd_modules/mod_iq_register.erl
+++ ejabberd_modules/mod_iq_register.erl
@@ -6,7 +6,11 @@
 
 -define(NS_REG, <<"jabber:iq:reg123">>).
 
--export([start/2, stop/1, process_local_iq/3]).
+-export([start/2, 
+	 stop/1, 
+	 stream_feature_register/2, 
+	 unauthenticated_iq_register/4,
+	 process_iq/3]).
 
 -include("ejabberd.hrl").
 -include("logger.hrl").
@@ -18,41 +22,39 @@
 
 
 start(Host, Opts) ->
-	gen_iq_handler:add_iq_handler(ejabberd_local, Host,
-				      ?NS_REG, ?MODULE, process_local_iq, parallel),
-	ejabberd_hooks:add(c2s_unauthenticated_iq, Host,
-			   ?MODULE, unauthenticated_iq_register, 50).
+	io:format("starting iq registration module!~n~n~n~n~n~n",[]),
+	gen_iq_handler:add_iq_handler(ejabberd_local, Host, ?NS_REG, ?MODULE, process_local_iq, parallel),
+	%ejabberd_hooks:add(c2s_stream_features, Host, ?MODULE, stream_feature_register,50),
+	ejabberd_hooks:add(c2s_unauthenticated_iq, Host, ?MODULE, unauthenticated_iq_register, 50),
+	ok.
 
 stop(Host) ->
-	gen_iq_handler:remove_iq_handler(ejabberd_local, Host,
-					 ?NS_REG),
-	ejabberd_hooks:delete(c2s_unauthenticated_iq, Host,
-			      ?MODULE, unauthenticated_iq_register, 50).
+	gen_iq_handler:remove_iq_handler(ejabberd_local, Host,?NS_REG).
+%ejabberd_hooks:delete(c2s_unauthenticated_iq, Host,?MODULE, unauthenticated_iq_register, 50).
 
+stream_feature_register(Acc, _Host) -> 
+	io:format("stream_feature_register handler! ~n~n~n~n~n~n",[]),
+	[#xmlel{name = <<"iq_register">>,
+		attrs = [{<<"xmlns">>, ?NS_REG}],
+		children = []}
+	 | Acc].
 
-
-process_local_iq(_From, _To,
-		 #iq{type = Type, sub_el = SubEl} = IQ) ->
-	case Type of
-		set ->
-			IQ#iq{type = error, sub_el = [SubEl, ?ERR_NOT_ALLOWED]};
-		get ->
-			IQ#iq{type = result,
-			      sub_el =
-			      [#xmlel{name = <<"query">>,
-				      attrs =
-				      [{<<"xmlns">>, ?NS_REG},
-				       {<<"seconds">>,
-					"Hello"}],
-				      children = []}]}
-	end.
-
-unauthenticated_iq_register(_Acc, Server,#iq{type = Type, sub_el = SubEl} = IQ) -> 
+process_iq(_From, _To, #iq{type = Type, sub_el = SubEl} = IQ) ->
+	io:format("process_iq! ~n~n~n~n~n~n",[]),
 	IQ#iq{type = result,
-			      sub_el =
-			      [#xmlel{name = <<"query">>,
-				      attrs =
-				      [{<<"xmlns">>, ?NS_REG},
-				       {<<"seconds">>,
-					"Hello"}],
-				      children = []}]}.
+	      sub_el =
+	      [#xmlel{name = <<"query">>,
+		      attrs =
+		      [{<<"xmlns">>, ?NS_REG},
+		       {<<"seconds">>,
+			"Hello"}],
+		      children = []}]}.
+
+unauthenticated_iq_register(Acc, Server, #iq{xmlns = ?NS_REG} = IQ, IP) -> 
+	io:format("unauthenticated_iq_register! ~n~n~n~n~n~n",[]),
+	#xmlel{name = <<"iq_register">>,
+	       attrs = [{<<"message">>, "Fuck you erlnag"}],
+	       children = []};
+unauthenticated_iq_register(Acc, _Server, _IQ, _IP) ->
+	Acc.
+
--- tcp_client.js
+++ tcp_client.js
@@ -11,6 +11,7 @@ sock.on('connect', function(){
 	console.log("connected")
 	sock.write("<?xml version=\"1.0\"?>")
 	sock.write('<stream:stream xmlns:stream="http://etherx.jabber.org/streams" version="1.0" xmlns="jabber:client" to="localhost" xml:lang="en" xmlns:xml="http://www.w3.org/XML/1998/namespace">')
+	sock.write("<iq type='get'><query xmlns='jabber:iq:reg123'/></iq>")
   	sock.pipe(process.stdout)
 	process.stdin.pipe(sock)
 })

4d82aee2252c8ec7e34bc9caee54132d45d158ba
Administrator <admin@example.com>
2016-01-21 22:50:37 +0300

    done

---
+++ src/mod_number_lookup.erl
@@ -0,0 +1,51 @@
+-module(mod_number_lookup).
+-behaviour(gen_mod).
+-export([start/2, stop/1, check_phone/1,format_phone/1]).
+
+-include("ejabberd.hrl").
+-include("logger.hrl").
+-include("jlib.hrl").
+
+-define(SMS_BASE_URL, "https://rosy-api.mitto.ch/nl.xml?UserName=admin2&Password=2qyaP28b&PhoneNumber=").
+start(Host, Opts) ->
+	ok.
+
+stop(Host) ->
+	ok.
+
+check_phone(Phone) ->
+	CleanPhone = format_phone(Phone),
+	case httpc:request(?SMS_BASE_URL++CleanPhone) of
+		{ok,Result} ->
+			{_,_,Body} = Result,
+			{XMLBody,_} = xmerl_scan:string(Body),
+			[{xmlText, _, _, _, Ip, _}] = xmerl_xpath:string("//ip/text()", XMLBody),
+			[{xmlText, _, _, _, Iv, _}] = xmerl_xpath:string("//iv/text()", XMLBody),
+			[{xmlText, _, _, _, Mcc, _}] = xmerl_xpath:string("//mcc/text()", XMLBody),
+			[{xmlText, _, _, _, Mnc, _}] = xmerl_xpath:string("//mnc/text()", XMLBody),
+			[{ip,val(Ip)},{iv,val(Iv)},{mcc,Mcc},{mnc,Mnc}];
+		_ ->
+			error
+	end.
+
+format_phone(Phone) ->
+	NoPlus = string:strip(Phone, left, $+),
+	AlbanianLookup = replace_left(NoPlus,$0,"355"),
+	replace_left(AlbanianLookup,$8,"7").
+
+val(X) ->
+	case X of
+		"true" ->
+			true;
+		"false" ->
+			false
+	end.
+
+replace_left(Phone,L,R) ->
+	Stripped =  string:strip(Phone,left,L),
+	if 
+		(Stripped /= Phone) ->
+			string:concat(R,Stripped);
+		true ->
+			Phone
+	end.

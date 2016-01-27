84af364697be51d4b0460b7947aad4e8cadef882
Administrator <admin@example.com>
2016-01-27 18:12:52 +0300

    Reworked

--- src/mod_iq_register.erl
+++ src/mod_iq_register.erl
@@ -7,7 +7,7 @@
 -define(NS_REG, <<"jabber:iq:register_phone">>).
 -define(NS_CONFIRM, <<"jabber:iq:confirm_phone">>).
 -define(DOMAIN, <<"localhost">>).
--define(SMS_BASE_URL, "http://10.20.254.4:13002/cgi-bin/sendsms?username=kanneluser&password=kannelpass&smsc=mitto&from=test&to=").
+-define(SMS_BASE_URL, "http://10.20.254.4:13002/cgi-bin/sendsms?username=kanneluser&password=kannelpass&smsc=mitto&from=Glomo.im&to=").
 -define(ALLOWED_CHARS, [$0,$1,$2,$3,$4,$5,$6,$7,$8,$9,$a,$b,$c,$d,$e,$f,$g,$h,$i,$j,$k,$l,$m,$n,$o,$p,$q,$r,$s,$t,$u,$v,$w,$x,$y,$z]).
 
 -export([start/2, 
@@ -21,10 +21,10 @@
 -include("jlib.hrl").
 -include("mod_privacy.hrl").
 
--record(sms_passwd, {phone = <<"">> :: binary() ,passwd = <<"">> :: binary()} ).
+-record(user_countries,{user= <<"">>, country = <<"">>}).
 
 random_char() ->
-	Val = rand:uniform(length(?ALLOWED_CHARS)),
+	Val = random:uniform(length(?ALLOWED_CHARS)),
 	lists:nth(Val, ?ALLOWED_CHARS).
 
 random_password(0) -> 
@@ -32,11 +32,8 @@ random_password(0) ->
 random_password(N) ->
 	[random_char()|random_password(N-1)].
 
-sms_passwd_schema() ->
-	{record_info(fields, sms_passwd), #sms_passwd{}}.
-
 start(Host, Opts) ->
-	mnesia:create_table(sms_passwd,[{attributes, record_info(fields, sms_passwd)}]),
+	mnesia:create_table(user_countries,[{attributes, record_info(fields, user_countries)}]),
 	ejabberd_hooks:add(c2s_unauthenticated_iq, Host, ?MODULE, unauthenticated_iq, 50),
 	inets:start(),
 	ok.
@@ -51,57 +48,64 @@ unauthenticated_iq(Acc, Server, #iq{xmlns = ?NS_REG, sub_el = SubEl} = IQ, IP) -
 		(PhoneTag /= false)->
 
 			{xmlel,_,_,PhoneChildren} = PhoneTag,
-			PhoneNumber = xml:get_cdata(PhoneChildren),
+			PhoneNumber = binary:bin_to_list(xml:get_cdata(PhoneChildren)),
 
-			NewPasswd = list_to_binary(random_password(10)),
+			FormattedPhone = mod_number_lookup:format_phone(PhoneNumber),
 
-			{atomic, ok} = ejabberd_auth:try_register(PhoneNumber, Server, NewPasswd),
-			jlib:iq_to_xml(IQ#iq{
+			NewPasswd = list_to_binary(random_password(3)),
+
+			UserExists = ejabberd_auth:is_user_exists(list_to_binary(FormattedPhone),Server),
+
+			if 
+				(UserExists == true) ->
+					ejabberd_auth:set_password(list_to_binary(FormattedPhone),Server,NewPasswd),
+					SmsUrl = ?SMS_BASE_URL ++ PhoneNumber ++ "&text=" ++ binary_to_list(NewPasswd),
+					{ok, {{Version, 202, ReasonPhrase}, Headers, Body}} = httpc:request(SmsUrl),
+					jlib:iq_to_xml(IQ#iq{
 					 type = result,
 					 sub_el = [
 						   #xmlel{
 						      name = <<"new-account">>, 
 						      attrs = [
-							       {<<"jid">>, jlib:jid_to_string(#jid{ user = PhoneNumber, server = Server})},
-							       {<<"password">>, NewPasswd}
+							       {<<"result">>,<<"ok">>}
 							      ] 
 						     }
 						  ]
 					});
-		true -> 
-			jlib:iq_to_xml(IQ#iq{type = error,sub_el = [SubEl, ?ERR_NOT_ALLOWED]})
-	end;
-
-unauthenticated_iq(Acc, Server, #iq{xmlns = ?NS_CONFIRM, sub_el = SubEl} = IQ, IP) ->
-	PasswdTag = xml:get_subtag(SubEl, <<"password">>),
-	PhoneTag = xml:get_subtag(SubEl, <<"phone">>),
-	if 
-		(PhoneTag /= false) and (PasswdTag /= false) ->
-
-			{xmlel,_,_,PhoneChildren} = PhoneTag,
-			PhoneNumber = xml:get_cdata(PhoneChildren),
-
-			{xmlel,_,_,PasswdChildren} = PasswdTag,
-			Passwd = xml:get_cdata(PasswdChildren),
-
-			case mnesia:dirty_read(sms_passwd,  PhoneNumber) of
-				[#sms_passwd{passwd = Passwd}] -> 
-					NewPasswd = list_to_binary(random_password(10)),
-
-					{atomic, ok} = ejabberd_auth:try_register(PhoneNumber, ?DOMAIN, NewPasswd),
-					jlib:iq_to_xml(IQ#iq{
+				true ->	 
+					CheckPhone = mod_number_lookup:check_phone(FormattedPhone),
+					[{ip,Ip},{iv,Iv},{mcc,Mcc},{mnc,Mnc}] = CheckPhone,
+					if
+						(Iv == true) ->
+							{atomic, ok} = ejabberd_auth:try_register(list_to_binary(FormattedPhone), Server, NewPasswd),
+							SmsUrl = ?SMS_BASE_URL ++ PhoneNumber ++ "&text=" ++ binary_to_list(NewPasswd),
+							{ok, {{Version, 202, ReasonPhrase}, Headers, Body}} = httpc:request(SmsUrl),
+							ok = mnesia:dirty_write(#user_countries{user = list_to_binary(FormattedPhone), country = list_to_binary(Mcc)}),
+							jlib:iq_to_xml(IQ#iq{
 							 type = result,
 							 sub_el = [
 								   #xmlel{
-								      name = <<"new_password">>, 
-								      attrs = [], 
-								      children = [{xmlcdata, NewPasswd}]
+								      name = <<"new-account">>, 
+								      attrs = [
+									       {<<"result">>,<<"ok">>}
+									      ] 
 								     }
 								  ]
 							});
-				_ ->
-					jlib:iq_to_xml(IQ#iq{type = error ,sub_el = []})
-			end;				
+						true ->
+							jlib:iq_to_xml(IQ#iq{
+							 type = error,
+							 sub_el = [
+								   #xmlel{
+								      name = <<"error">>, 
+								      attrs = [
+									       {<<"reason">>,<<"not valid phone number">>}
+									      ] 
+								     }
+								  ]
+							})
+					end
+			end;
 		true -> 
 			jlib:iq_to_xml(IQ#iq{type = error,sub_el = [SubEl, ?ERR_NOT_ALLOWED]})
 	end;

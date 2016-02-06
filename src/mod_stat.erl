12030ece7039126eec413d46790114702aea1359
gcorn <michael.enco@gmail.com>
2016-02-06 22:10:08 +0000

    user and sms stats

--- src/mod_stat.erl
+++ src/mod_stat.erl
@@ -5,11 +5,12 @@
 -export([start/2, stop/1, 
 	 register_user/2, 
 	 init_mcc_table/1, 
-	 user_sent_sms/1, 
+	 user_sent_sms/2, 
 	 close_session/4, 
 	 http_upload_slot_request/4,
 	 webadmin_json_api/2,
-	 init_test_stat_data/1]).
+	 add_stat_users/1,
+	 add_stat_sms/1]).
 
 -include("ejabberd.hrl").
 -include("logger.hrl").
@@ -32,7 +33,8 @@ start(Host, _Opts) ->
 			[{disc_copies, [node()]},
 			 {attributes, record_info(fields, mcc_country)}]),
     mnesia:create_table(stat_sms, 
-			[{disc_copies, [node()]},
+			[{type,bag},
+			 {disc_copies, [node()]},
 			 {attributes, record_info(fields, stat_sms)}]),
     mnesia:create_table(stat_register, 
 			[{disc_copies, [node()]},
@@ -42,16 +44,15 @@ start(Host, _Opts) ->
 			 {attributes, record_info(fields, stat_file)}]),
 
     ejabberd_hooks:add(register_user, Host, ?MODULE, register_user, 50),
-    ejabberd_hooks:add(user_sent_sms, Host, ?MODULE, user_sent_sms, 50),
+    ejabberd_hooks:add(sms_sent, Host, ?MODULE, user_sent_sms, 50),
     ejabberd_hooks:add(close_session, Host, ?MODULE, close_session, 50),
-
     ejabberd_hooks:add(webadmin_json_api, ?MODULE, webadmin_json_api, 50),
     ok.
 
 stop(Host) ->
     ejabberd_hooks:delete(register_user, Host, ?MODULE, register_user, 50),
     ejabberd_hooks:delete(close_session, Host, ?MODULE, close_session, 50),
-    ejabberd_hooks:delete(user_sent_sms, Host, ?MODULE, user_sent_sms, 50),
+    ejabberd_hooks:delete(sms_sent, Host, ?MODULE, user_sent_sms, 50),
 
     ejabberd_hooks:delete(webadmin_json_api, ?MODULE, webadmin_json_api, 50),
     ok.
@@ -60,26 +61,37 @@ now_to_timestamp({MegaSec, Sec, MiliSec}) ->
     MegaSec*1000000000+Sec*1000+(MiliSec div 1000).
 
 webadmin_json_api(Acc, #request{path = [<<"api">>, <<"stat">>]}) -> 
-    Fun = fun() -> 
-		  Q = qlc:q([
-			     [{<<"user">>,User},
-			      {<<"timestamp">>,now_to_timestamp(Now)},
-			      {<<"country">>, Country},
-			      {<<"last_activity">>, LT*1000}
-			     ] ||
-			     #stat_register{user=User, timestamp=Now} <- mnesia:table(stat_register),
-			     #user_countries{user=CUser, country=CMcc} <- mnesia:table(user_countries),
-			     User == CUser,
-			     #mcc_country{mcc=Mcc, country=Country} <- mnesia:table(mcc_country),
-			     Mcc == CMcc,
-			     #last_activity{us={LU,_}, timestamp=LT} <- mnesia:table(last_activity),
-			     User == LU
-			    ]),	
-		  qlc:e(Q)
-	  end,
-    {atomic, Res} = mnesia:transaction(Fun),
-    io:format("~nregs count: ~p",[length(Res)]),
-    {stop, Res}.
+    UserQuery = fun() -> 
+	  Q = qlc:q([
+		     [{<<"user">>,User},
+		      {<<"timestamp">>,now_to_timestamp(Now)},
+		      {<<"country">>, Country},
+		      {<<"last_activity">>, LT*1000}] ||
+		     #stat_register{user=User, timestamp=Now} <- mnesia:table(stat_register),
+		     #user_countries{user=CUser, country=CMcc} <- mnesia:table(user_countries),
+		     User == CUser,
+		     #mcc_country{mcc=Mcc, country=Country} <- mnesia:table(mcc_country),
+		     Mcc == CMcc,
+		     #last_activity{us={LU,_}, timestamp=LT} <- mnesia:table(last_activity),
+		     User == LU]),	
+	  qlc:e(Q)
+    end,
+    SmsQuery = fun() ->
+	  Q = qlc:q([
+		   [{<<"timestamp">>, now_to_timestamp(Now)},
+		    {<<"country">>, Country}] ||  
+		    #stat_sms{user=User, timestamp=Now} <-mnesia:table(stat_sms),
+		    #user_countries{user=CU, country=CMcc} <-mnesia:table(user_countries),
+		    User == CU,
+		    #mcc_country{mcc=Mcc, country=Country} <- mnesia:table(mcc_country),
+		    Mcc==CMcc]),
+	  qlc:e(Q)
+    end,
+    {atomic, Users} = mnesia:transaction(UserQuery),
+    {atomic, Sms} = mnesia:transaction(SmsQuery),
+    {stop, [{<<"users">>, Users},
+	    {<<"sms">>, Sms}
+	   ]}.
 
 register_user(User, Server) ->
     mnesia:dirty_write(#stat_register{user=User, timestamp=os:timestamp()}),
@@ -89,8 +101,8 @@ register_user(User, Server) ->
 				      status= <<"registered">>}),
     ok.
 
-user_sent_sms({User, Server}) ->
-    mnesia:dirty_write(#stat_sms{user={User,Server}, timestamp=os:timestamp()}),
+user_sent_sms(From, _To) ->
+    mnesia:dirty_write(#stat_sms{user=From, timestamp=os:timestamp()}),
     ok.
 
 close_session({SessionStart, _}, #jid{user=User, server=Server},_, _) ->
@@ -100,7 +112,6 @@ close_session({SessionStart, _}, #jid{user=User, server=Server},_, _) ->
 
 http_upload_slot_request(_,_,_,_) -> ok.
 
-
 init_mcc_table(FilePath) ->
     {ok, XmlString} = file:read_file(FilePath),
     #xmlel{children=Carriers} = xml_stream:parse_element(XmlString),
@@ -108,9 +119,13 @@ init_mcc_table(FilePath) ->
     MccCountry = lists:filtermap(fun (X) ->
 					 case X of 
 					     #xmlel{} ->
-						 Country = xml:get_path_s(X, [{elem, <<"country">>}, cdata]),
+						 CountryBin = xml:get_path_s(X, [{elem, <<"country">>}, cdata]),
+						 TrimmedCountryStr = re:replace(binary_to_list(CountryBin),
+									       "(^\\s+)|(\\s+$)",
+									       "", 
+									       [global,{return,list}]),
 						 Mcc = xml:get_path_s(X, [{elem, <<"mcc">>}, cdata]),
-						 {true,{Country,Mcc}};
+						 {true,{list_to_binary(TrimmedCountryStr),Mcc}};
 					     _ -> false
 					 end
 				 end,
@@ -122,12 +137,15 @@ init_mcc_table(FilePath) ->
 		  UniqMccCountry),
     ok.
 
+
+%%%================================
+%% generate test data for dashboard
 random_mcc() ->
     Keys = mnesia:dirty_all_keys(mcc_country),
     lists:nth(random:uniform(length(Keys)),Keys).
 
-init_test_stat_data(1) -> ok;
-init_test_stat_data(Num) ->
+add_stat_users(0) -> ok;
+add_stat_users(Num) ->
     User = list_to_binary([random:uniform(9)+48 || _ <- lists:seq(1,11)]),
     Mcc = random_mcc(),
     SecInMonth = 31*24*60*60,
@@ -139,5 +157,23 @@ init_test_stat_data(Num) ->
     mnesia:dirty_write(#last_activity{us={User,<<"localhost">>}, 
 				      timestamp=now_to_timestamp(RegDate) div 1000, 
 				      status= <<"registered">>}),
-    init_test_stat_data(Num-1).
+    add_stat_users(Num-1).
 
+random_stat_user() ->
+    Keys = mnesia:dirty_all_keys(stat_register),
+    lists:nth(random:uniform(length(Keys)),Keys).
+
+add_stat_sms(0) -> ok;
+add_stat_sms(Num) ->
+    User = random_stat_user(),
+    {MSec, Sec, _} = os:timestamp(),
+    [#stat_register{timestamp={MSec1, Sec1, _}}] = mnesia:dirty_read(stat_register, User),
+    Period = (MSec-MSec1)*1000000 + (Sec-Sec1),
+    Tmp = MSec*1000000+Sec-random:uniform(Period),
+    Ts = {Tmp div 1000000, Tmp rem 1000000, 0},
+
+    mnesia:dirty_write(#stat_sms{
+			  user=random_stat_user(),
+			  timestamp = Ts}),
+    add_stat_sms(Num-1).
+            
--- web_admin/stat.js
+++ web_admin/stat.js
@@ -2,60 +2,152 @@ var req = new XMLHttpRequest()
 req.open('GET', '/admin/api/stat', false)
 req.send()
 data = JSON.parse(req.responseText)
-
+var userData = data.users
+var smsData=data.sms
 var total_regs = data.length
 
-Array.prototype.forEach.call(data, function(d,i) {
+
+var date_formatter = d3.time.format("%d.%m.%y")	
+var countryColors = d3.scale.category20()
+
+Array.prototype.forEach.call(userData, function(d,i) {
     d.date = new Date(d.timestamp)
 })
-
-var regs = crossfilter(data)
-
-var active_users = crossfilter(
-    Array.prototype.filter.call(data, function(d, i) {
-	return (Date.now() - d.timestamp < 10*24*60*60*1000)
-    }))
-
+Array.prototype.forEach.call(smsData, function(d,i) {
+    d.date = new Date(d.timestamp)
+})
+var dataset ={
+    reg: crossfilter(userData),
+    active_users: crossfilter(
+	Array.prototype.filter.call(userData, function(d, i) {
+	    return (Date.now() - d.timestamp < 10*24*60*60*1000)
+	})),
+    sms: crossfilter(smsData)
+}
 var dim = {
-    date: regs.dimension(function(d) {return d.date}),
-    country: regs.dimension(function(d) {return d.country}),
-    active: active_users.dimension(function(d) {return d.country})
-}
-var group = {
-    date: dim.date.group(d3.time.day),
-    country: dim.country.group(),
-    active: dim.active.group()
+    date: dataset.reg.dimension(function(d) {return d.date}),
+    country: dataset.reg.dimension(function(d) {return d.country}),
+    active: dataset.active_users.dimension(function(d) {return d.country}),
+    sms_country: dataset.sms.dimension(function(d) {return d.country}),
+    sms_date: dataset.sms.dimension(function(d) {return d.date}),
 }
 
-var countryColors = d3.scale.category20c()
+var first_user_date = dim.date.bottom(1).length?dim.date.bottom(1)[0].date:new Date()
 
-var pie_charts  = {
-    reg_chart: {
+var date_interval = [
+    new Date(first_user_date.getTime()-24*60*60*1000),
+    new Date()
+]
+
+var filters = {
+    country: [],
+    date: date_interval.map(function(d) {
+	return new Date(d.getTime())
+    })
+}
+
+//select2 initialization
+var select_data = dim.country.group().all()
+    .map(function(d) {
+	return {id: d.key, text: d.key}
+    })
+var select2 = $('#select2').select2({
+    placeholder: "All countries",
+    data: select_data
+})
+
+select2.on('change', function(e) {
+    var res = select2.val() || []
+    filters.country = res
+    apply_filters()
+})
+/////////////////////////////////
+//date filters initialization
+var date_input1=$('#d1').datepicker({
+	dateFormat: "dd.mm.y",
+	minDate: date_interval[0],
+	maxDate: date_interval[1]
+    })
+    .datepicker('setDate',date_interval[0])
+    .on('change', function() {
+	filters.date[0] = $(this).datepicker('getDate')
+	apply_filters()
+    })
+
+var date_input2=$('#d2').datepicker({
+	dateFormat:"dd.mm.y",
+	minDate: date_interval[0],
+	maxDate: date_interval[1]
+    })
+    .datepicker('setDate',date_interval[1])
+    .on('change', function() {
+	filters.date[1] = $(this).datepicker('getDate')
+	apply_filters()
+    })
+////////////////////////////////////
+var charts = [
+    pieChart({
 	selector: '#reg-chart',
 	dim: dim.country,
-	group: group.country,
 	colors: countryColors
-    },
-    active_chart: {
+    }),
+    pieChart({
 	selector: '#active-chart',
 	dim: dim.active,
-	group: group.active,
 	colors: countryColors
-    }
-	
+    }),
+    pieChart({
+	selector: "#sms-chart",
+	dim: dim.sms_country,
+	colors: countryColors
+    }),
+    lineChart({
+	selector: "#reg-timeline",
+	dim: [dim.sms_date, dim.date],
+	labels: ['registered_users', 'sms'],
+	colors: countryColors
+    })
+]
+function redraw() {
+    charts.forEach(function(d) { d.redraw()})
 }
+redraw()
+//////////////////////////////////////
+function apply_filters() {
+    function countryFilter(d) {
+	if(filters.country.length)
+	    return filters.country.indexOf(d)>=0
+	else return true
+    }
 
+    select2.val(filters.country)
+	.trigger('change.select2')
+    date_input1.datepicker('setDate',filters.date[0])
+    date_input2.datepicker('setDate',filters.date[1])
+
+    dim.country.filter(countryFilter)
+    dim.active.filter(countryFilter)
+    dim.sms_country.filter(countryFilter)
+
+    dim.date.filter(filters.date)
+    dim.sms_date.filter(filters.date)
+
+    redraw()
+}
+/////////////////////////////////////
 function pieChart(opt) {
     var radius = 125
     var width = 250
     var height = 250
-    var groupData = opt.group
-    var dimData = opt.dim
+    var dim = opt.dim
     var selector = opt.selector
     var scale = opt.colors
 
-    var total = function() {
-	return dimData.groupAll().value()
+    var total = function () {
+	return dim.groupAll().value()
+    }
+    var filtered_total = function() {
+	return dim.top(Infinity).length 
     }
 
     var tooltip = d3.select(selector)
@@ -82,234 +174,273 @@ function pieChart(opt) {
     var pie = d3.layout.pie()
 	.value(function(d) { return d.value })
 	.sort(null)
-
-    var path = svg.selectAll('path')
-	.data(pie(groupData.all()))
-        .enter()
-	.append('path')
-	.attr('d', arc)
-	.attr('fill', function(d, i) { 
-	    return scale(d.data.key)
-	})
-
+    
     var totalText = svg.append('svg:text')
 	.attr('class', 'total')
 	.attr('text-anchor','middle')
 	.attr('alignment-baseline','middle')
 	.attr('font-size',radius/3+'px')
-	.text(total())
 
-    path.on('mouseover', function(d) {
-	var percent = Math.round(1000*d.data.value/total())/10
+    var path = svg.selectAll('path')
+	
+    return {
+	redraw: function() {
+	    path = path.data(pie(dim.group().all()))
+		.attr('d', arc)
+		.attr('class', function(d) {
+		    var cls = 'pie-segment '
+		    if(filters.country.length) {
+			if(filters.country.indexOf(d.data.key)>=0) {
+			    cls+="active"
+			} else {
+			    cls+='inactive'
+			}
+		    }
+		    return cls
+		})
 
-	tooltip.select('.label').html(d.data.key)
-	tooltip.select('.count').html(d.data.value)
-	tooltip.select('.percent').html(percent+"%")
-	tooltip.style('display','block')
-    })
+	    path
+		.enter()
+		.append('path')
+		.attr('class', function(d) {
+		    var cls = 'pie-segment '
+		    if(filters.country.length) {
+			if(filters.country.indexOf(d.data.key)>=0) {
+			    cls+="active"
+			} else {
+			    cls+='inactive'
+			}
+		    }
+		    return cls
+		})
+		.attr('fill', function(d) { 
+		    return scale(d.data.key)
+		})
+		.attr('d', arc)
 
-    path.on('mouseout', function() {	
-	tooltip.style('display', 'none')	
-    })
-    path.on('mousemove', function(d) {
-	tooltip.style('top', (d3.event.layerY + 10) + 'px')
-	    .style('left', (d3.event.layerX + 10) + 'px')
-    });
-}
+	    path.exit().remove()
 
-pieChart(pie_charts.reg_chart)
-pieChart(pie_charts.active_chart)
+	    totalText.text(filtered_total())
 
-/*var charts = [
-    barChart()
-	.dimension(dim.date)
-	.group(group.date)
-	.x(d3.time.scale()
-		.domain([start, end])
-		.rangeRound([1,500]))
-]
+	    path.on('mouseover', function(d) {
+		var percent = Math.round(1000*d.data.value/total())/10
 
-var chart = d3.selectAll(".chart")
-    .data(charts)
-    .each(function(chart) { 
-	chart.on("brush", renderAll)
-	    .on("brushend", renderAll); 
-    })
+		tooltip.select('.label').html(d.data.key)
+		tooltip.select('.count').html(d.data.value)
+		tooltip.select('.percent').html(percent+"%")
+		tooltip.style('display','block')
+	    })
 
-renderAll()
+	    path.on('mouseout', function() {	
+		tooltip.style('display', 'none')	
+	    })
+	    path.on('mousemove', function(d) {
+		tooltip.style('top', (d3.event.layerY + 10) + 'px')
+		    .style('left', (d3.event.layerX + 10) + 'px')
+	    });
 
-function render(method) {
-    d3.select(this).call(method);
-}
-
-function renderAll() {
-    chart.each(render)
-}
-
-function barChart() {
-    if (!barChart.id) barChart.id = 0;
-    var margin = {top: 10, right: 10, bottom: 20, left: 10},
-	x,
-	y = d3.scale.linear().range([100, 0]),
-	id = barChart.id++,
-	axis = d3.svg.axis().orient("bottom"),
-	brush = d3.svg.brush(),
-	brushDirty,
-	dimension,
-	group,
-	round;
-    function chart(div) {
-	var width = x.range()[1],
-	height = y.range()[0];
-	y.domain([0, group.top(1)[0].value]);
-	div.each(function() {
-	    var div = d3.select(this),
-	    g = div.select("g");
-	    // Create the skeletal chart.
-	    if (g.empty()) {
-		div.select(".title").append("a")
-		    .attr("href", "javascript:reset(" + id + ")")
-		    .attr("class", "reset")
-		    .text("reset")
-		    .style("display", "none");
-		g = div.append("svg")
-		    .attr("width", width + margin.left + margin.right)
-		    .attr("height", height + margin.top + margin.bottom)
-		    .append("g")
-		    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-		g.append("clipPath")
-		    .attr("id", "clip-" + id)
-		    .append("rect")
-		    .attr("width", width)
-		    .attr("height", height);
-		g.selectAll(".bar")
-		    .data(["background", "foreground"])
-		    .enter().append("path")
-		    .attr("class", function(d) { return d + " bar"; })
-		    .datum(group.all());
-		g.selectAll(".foreground.bar")
-		    .attr("clip-path", "url(#clip-" + id + ")");
-		g.append("g")
-		    .attr("class", "axis")
-		    .attr("transform", "translate(0," + height + ")")
-		    .call(axis);
-		// Initialize the brush component with pretty resize handles.
-		var gBrush = g.append("g").attr("class", "brush").call(brush);
-		gBrush.selectAll("rect").attr("height", height);
-		gBrush.selectAll(".resize").append("path").attr("d", resizePath);
-	    }
-	    // Only redraw the brush if set externally.
-	    if (brushDirty) {
-		brushDirty = false;
-		g.selectAll(".brush").call(brush);
-		div.select(".title a").style("display", brush.empty() ? "none" : null);
-		if (brush.empty()) {
-		    g.selectAll("#clip-" + id + " rect")
-			.attr("x", 0)
-			.attr("width", width);
+	    path.on('click', function(d) {
+		var index = filters.country.indexOf(d.data.key)
+		if(index>=0) {
+		    filters.country.splice(index,1)
 		} else {
-		    var extent = brush.extent();
-		    g.selectAll("#clip-" + id + " rect")
-			.attr("x", x(extent[0]))
-			.attr("width", x(extent[1]) - x(extent[0]));
+		    filters.country.push(d.data.key)
 		}
-	    }
-	    g.selectAll(".bar").attr("d", barPath);
-	});
-	function barPath(groups) {
-	    var path = [],
-	    i = -1,
-	    n = groups.length,
-	    d;
-	    while (++i < n) {
-		d = groups[i];
-		path.push("M", x(d.key), ",", height, "V", y(d.value), "h9V", height);
-	    }
-	    return path.join("");
-	}
-	function resizePath(d) {
-	    var e = +(d == "e"),
-	    x = e ? 1 : -1,
-	    y = height / 3;
-	    return "M" + (.5 * x) + "," + y
-		+ "A6,6 0 0 " + e + " " + (6.5 * x) + "," + (y + 6)
-		+ "V" + (2 * y - 6)
-		+ "A6,6 0 0 " + e + " " + (.5 * x) + "," + (2 * y)
-		+ "Z"
-		+ "M" + (2.5 * x) + "," + (y + 8)
-		+ "V" + (2 * y - 8)
-		+ "M" + (4.5 * x) + "," + (y + 8)
-		+ "V" + (2 * y - 8);
+		apply_filters()
+	    })
 	}
     }
-    brush.on("brushstart.chart", function() {
-	var div = d3.select(this.parentNode.parentNode.parentNode);
-	div.select(".title a").style("display", null);
-    });
-    brush.on("brush.chart", function() {
-	var g = d3.select(this.parentNode),
-	extent = brush.extent();
-	if (round) g.select(".brush")
-	    .call(brush.extent(extent = extent.map(round)))
-		.selectAll(".resize")
-		.style("display", null);
-	g.select("#clip-" + id + " rect")
-	    .attr("x", x(extent[0]))
-	    .attr("width", x(extent[1]) - x(extent[0]));
-	dimension.filterRange(extent);
-    });
-    brush.on("brushend.chart", function() {
-	if (brush.empty()) {
-	    var div = d3.select(this.parentNode.parentNode.parentNode);
-	    div.select(".title a").style("display", "none");
-	    div.select("#clip-" + id + " rect").attr("x", null).attr("width", "100%");
-	    dimension.filterAll();
-	}
-    });
-    chart.margin = function(_) {
-	if (!arguments.length) return margin;
-	margin = _;
-	return chart;
-    };
-    chart.x = function(_) {
-	if (!arguments.length) return x;
-	x = _;
-	axis.scale(x);
-	brush.x(x);
-	return chart;
-    };
-    chart.y = function(_) {
-	if (!arguments.length) return y;
-	y = _;
-	return chart;
-    };
-    chart.dimension = function(_) {
-	if (!arguments.length) return dimension;
-	dimension = _;
-	return chart;
-    };
-    chart.filter = function(_) {
-	if (_) {
-	    brush.extent(_);
-	    dimension.filterRange(_);
-	} else {
-	    brush.clear();
-	    dimension.filterAll();
-	}
-	brushDirty = true;
-	return chart;
-    };
-    chart.group = function(_) {
-	if (!arguments.length) return group;
-	group = _;
-	return chart;
-    };
-    chart.round = function(_) {
-	if (!arguments.length) return round;
-	round = _;
-	return chart;
-    };
-    return d3.rebind(chart, brush, "on");
-}*/
+} 
+///////////////////////////////////////
+function lineChart(opt) {
+    var selector = opt.selector
+    var dims = opt.dim
+    var labels = opt.labels
+
+    var margin = {
+	top: 20,
+	bottom: 80,
+	left: 40,
+	right: 20
+    }
+    
+    var width = 800-margin.left-margin.right
+    var height = 400-margin.top-margin.bottom
 
 
+    var scale = {
+	x: d3.time.scale()
+	    .domain(date_interval)
+	    .range([0, width]),
+	y: d3.scale.linear()
+	    .range([height,0])
+    }
+
+    var axis = {
+	x: d3.svg.axis().scale(scale.x)
+	    .orient("bottom")
+	    .ticks(10),
+	y: d3.svg.axis().scale(scale.y)
+	    .orient('left')
+	    .ticks(5)
+    }
+
+    var svg = d3.select(selector)
+	.append('svg')
+	.attr('width',width+margin.left+margin.right)
+	.attr('height',height+margin.top+margin.bottom)
+	.append('g')
+	    .attr("transform", "translate("+margin.left+","+margin.top+")")
+
+    svg.append('g')
+	.attr('class', 'x axis')
+	.attr("transform", "translate(0," + height + ")")
+	.call(axis.x)
+
+    svg.append('g')
+	.attr('class', 'y axis')
+	.call(axis.y)
+
+    var line_gen = d3.svg.line()
+	.x(function(d) {return scale.x(d.key)})
+	.y(function(d) {return scale.y(d.value)})
+	//.interpolate('basis')
+
+    var brush = d3.svg.brush()
+	.x(scale.x)
+	.on('brush',function() {
+	    debugger
+	    extent1 = brush.extent().map(d3.time.day.round)
+	    d3.select(this)
+		.call(brush.extent(extent1))
+	    if(extent1[0]-extent1[1]==0) {
+		filters.date = date_interval
+	    } else {
+		filters.date = extent1
+	    }
+	    apply_filters()
+	})
+
+    svg.append('g')
+	.attr('class','brush')
+	.call(brush)
+	.selectAll('rect')
+	    .attr("y", -6)
+	    .attr("height", height + 7)
+
+    var tooltip = d3.select(selector)
+	.append('div')
+	.attr('class','tooltip')
+    tooltip.append('div')
+	.attr('class','label')
+
+    labels.forEach(function(d) {
+	tooltip.append('div')
+	    .attr('class',d)
+    })
+
+    var tooltip_line = svg.append('line')
+	.attr('class', 'tooltip-line')
+	.attr('y1',0)
+	.attr('y2', height)
+   
+    var mouse_catcher = svg.append('rect')
+	.attr('width',width)
+	.attr('height', height)
+	.style('fill','none')
+	.style('pointer-events','all')
+	//propagate mouse events to brush under catcher
+	.on('mousedown', function() {
+	    brush_elm = svg.select(".brush").node()
+	    new_click_event = new Event('mousedown')
+	    new_click_event.pageX = d3.event.pageX
+	    new_click_event.clientX = d3.event.clientX
+	    new_click_event.pageY = d3.event.pageY
+	    new_click_event.clientY = d3.event.clientY
+	    brush_elm.dispatchEvent(new_click_event)
+	})
+	.on('mouseover', function() {
+	    tooltip.style('display', 'block')
+	    tooltip_line.style('display', 'block')
+	})
+	.on('mouseout', function() {
+	    tooltip.style('display', 'none')
+	    tooltip_line.style('display', 'none')
+	})
+	.on('mousemove', function() {
+	    var hovered_date = scale.x.invert(d3.mouse(this)[0])
+	    var hovered_day = d3.time.day(hovered_date)
+	    var values = dims.map(function(d) {
+		var el = d.group(d3.time.day)
+		    .all()
+		    .find(function(d) { 
+			return d.key-hovered_day==0 
+		    })
+
+		
+		return el?el.value:0
+	    })
+
+	    tooltip.select('.label').html(date_formatter(hovered_day))
+	    labels.forEach(function (d,i) {
+		tooltip.select('.'+d)
+		    .html(d+": "+values[i])
+	    })
+    
+	    tooltip.style('display','block')
+	   
+	    tooltip_line.attr('x1',d3.mouse(this)[0]) 
+	    tooltip_line.attr('x2',d3.mouse(this)[0])
+
+	    tooltip.style('top', (d3.event.layerY + 10) + 'px')
+		.style('left', (d3.event.layerX + 10) + 'px')
+	    return true
+	})
+
+    return {
+	redraw: function() {
+	    var max_counts = dims.map(function(d) {
+		var tmp = d.group(d3.time.day).top(1) 
+		return tmp.length?tmp[0].value:0
+	    })
+	    var y_domain = [
+		0,
+		Math.max.apply(null,max_counts)
+	    ]
+	    scale.y.domain(y_domain)
+
+	    svg.select('.x.axis')
+		.call(axis.x) 
+		.selectAll("text")  
+		    .style("text-anchor", "end")
+		    .attr("dx", "-.8em")
+		    .attr("dy", ".15em")
+		    .attr("transform", "rotate(-65)" )
+
+	    svg.select('.y.axis')
+		.call(axis.y)
+
+	    var global_counter = 0
+	    var path = svg.selectAll('.line')
+		.data(dims.map(function(d) {
+			return d.group(d3.time.day).all()
+		    }))
+		.attr('d', line_gen)
+	    path.enter()
+		    .append('path')
+		    .attr('class','line')
+		    .attr('d', function(d) {
+			return line_gen(d)
+		    })
+		    .style('stroke', function(d) {
+			var clr = opt.colors(labels[global_counter])
+			global_counter++
+			return clr
+		    })
+	    path.exit().remove()
+	    if(!(filters.date[0]-date_interval[0]==0)
+	       || !(filters.date[1]-date_interval[1]==0))
+	    svg.select('.brush').call(brush.extent(filters.date))
+	}
+    }
+}
